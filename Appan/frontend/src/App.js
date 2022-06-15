37641b784b4d59879f71a9e230b160dfa163641f
Juan Felipe López <juanfeloor@gmail.com>
2022-06-15 17:40:28 -0500

    first

---
+++ Appan/frontend/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Pan de la Casa </a>
+      </header>
+      <main>
+        Lista de productos
+      </main>
+  
+    </div>
+  );
+}
+
+export default App;
